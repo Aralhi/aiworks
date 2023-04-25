4a5f89dacf03fa41855c9008f34748d60f82a11e
卡二 <kaer.zzp@alibaba-inc.com>
2023-04-25 11:20:09 +0800

    feat: move chatgpt service

--- pages/api/chatgpt/get.ts
+++ pages/api/chatgpt/get.ts
@@ -1,4 +1,4 @@
-import { OpenAIStream, OpenAIStreamPayload } from "../../utils/OpenAIStream";
+import { OpenAIStream, OpenAIStreamPayload } from "../../../utils/OpenAIStream";
 
 if (!process.env.OPENAI_API_KEY) {
   throw new Error("Missing env var from OpenAI");
@@ -9,7 +9,6 @@ export const config = {
 };
 
 const handler = async (req: Request): Promise<Response> => {
-  console.log('...req', req)
   const { prompt } = (await req.json()) as {
     prompt?: string;
   };
