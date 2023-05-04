5b4986e762113bb220a0a1089d462caa7343159c
卡二 <kaer.zzp@alibaba-inc.com>
2023-05-04 22:47:10 +0800

    feat: add home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,14 @@ import { useRef, useState } from "react";
 import { Toaster, toast } from "react-hot-toast";
 import Footer from "../components/Footer";
 import LoadingDots from "../components/LoadingDots";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   const [loading, setLoading] = useState(false);
   const [prompt, setPrompt] = useState("");
   const [completion, setCompletion] = useState<String>("");
+  const [chatText, setChatText] = useState<String>("免费试用");
+  const [mjText, setMjText] = useState<String>("免费试用");
 
   const bioRef = useRef<null | HTMLDivElement>(null);
 
@@ -58,112 +61,63 @@ const Home: NextPage = () => {
   };
 
   return (
-    <div className="">
-      <main>
-        <div className="container mx-auto py-10">
-          <h1 className="text-4xl font-bold mb-4">欢迎来到AI平台</h1>
-          <p className="text-gray-600 mb-4">
-            我们提供最先进的人工智能技术，帮助你实现业务的数字化转型。
-          </p>
-          <div className="grid grid-cols-2 gap-8 mb-10">
-            <div className="card">
-              <h2 className="font-bold mb-2">特色功能1</h2>
-              <p className="text-gray-600 mb-4">
-                我们提供最先进的人工智能技术，帮助你实现业务的数字化转型。
-              </p>
-              <a href="#" className="btn">
-                了解更多
-              </a>
-            </div>
-            <div className="card">
-              <h2 className="font-bold mb-2">特色功能2</h2>
-              <p className="text-gray-600 mb-4">
-                我们提供最先进的人工智能技术，帮助你实现业务的数字化转型。
-              </p>
-              <a href="#" className="btn">
-                了解更多
-              </a>
-            </div>
+    <main className="w-fulL">
+      <div id="website-desc" className="bg-[url('/bg.jpg')] text-white bg-cover md:pl-6 lg:pl-2" style={{ height: '400px' }}>
+        <h1 className="text-4xl w-full font-bold mb-4 pt-[150px]">AI works，让AI触手可及</h1>
+        <h2 className="text-2xl mb-4">
+          AI生产力工具伴随您的日常工作与生活
+        </h2>
+      </div>
+      <div id="chatgpt" className="md:flex items-start items-center" style={{
+        margin: '20px',
+        backgroundColor: 'hsla(210, 8.33%, 95.29%, 1)'
+      }}>
+        <div className="bg-[url('/chatgpt-desc.png')] bg-container md:w-2/3 bg-size: 100% 100%" style={{ height: '300px', backgroundSize: '100% 100%' }}></div>
+        <div className="flex justify-center items-center flex-col gap-4 p-4 md:w-1/3">
+          <h1 className="text-4xl w-full font-bold mb-4 text-center" style={{ color: 'rgb(52, 0, 104)' }}>chatGPT</h1>
+          <p style={{ color: 'rgb(52, 0, 104)' }}>全球最强大的人工智能，在创作、文案编辑、编码、美食配方等方面可以给你惊人的灵感和效率，更可以作为AIGC工具强大的prompt生成器。</p>
+          <Link className="text-green-400 underline" href={'/chat'}>{mjText}</Link>
+        </div>
+      </div>
+      <div id="mj-desc" className="text-gray-700 flex justify-center items-center flex-col gap-4" style={{ height: '400px', position: 'relative'}}>
+        <div className="bg-[url('/mj-desc.png')] bg-cover hover:opacity-30 z-0 w-full h-full" style={{ position: 'absolute' }}></div>
+        <p className="text-gray-200 text-3xl z-10 text-center">全球最强文生图模型，一分钟帮你实现创意。<br></br><Link className="text-green-400 text-3xl underline" href={'/midjourney'}>{mjText}MidJourney</Link></p>
+      </div>
+      <div id="feature" className="mt-6 mb-6 pl-6 pr-6">
+        <h1 className="text-4xl text-center mb-6">Feature</h1>
+        <div className="grid grid-cols-3 gap-8 mb-10">
+          <div className="flex flex-col justify-center items-center">
+            <img
+              src="/f3.png"
+              className="mb-4"
+            />
+            <p className="text-gray-600 mb-4">
+              用chatGPT帮你写代码，一分钟搞定技术难点，再也不用各种github、stack overflow乱搜啦！
+            </p>
+            <Link className="underline text-center" href={'/chat'}>了解更多</Link>
           </div>
-
-          <div className="grid grid-cols-3 gap-8 mb-10">
-            <div className="reference">
-              <img
-                src="https://via.placeholder.com/500x300"
-                alt="Reference 1"
-                className="mb-4"
-              />
-              <h3 className="font-bold mb-2">参考案例1</h3>
-              <p className="text-gray-600 mb-4">
-                我们帮助客户实现了业务数字化转型，取得了巨大的成功。
-              </p>
-              <a href="#" className="btn">
-                了解更多
-              </a>
-            </div>
-            <div className="reference">
-              <img
-                src="https://via.placeholder.com/500x300"
-                alt="Reference 2"
-                className="mb-4"
-              />
-              <h3 className="font-bold mb-2">参考案例2</h3>
-              <p className="text-gray-600 mb-4">
-                我们帮助客户实现了业务数字化转型，取得了巨大的成功。
-              </p>
-              <a href="#" className="btn">
-                了解更多
-              </a>
-            </div>
-            <div className="reference">
-              <img
-                src="https://via.placeholder.com/500x300"
-                alt="Reference 3"
-                className="mb-4"
-              />
-              <h3 className="font-bold mb-2">参考案例3</h3>
-              <p className="text-gray-600 mb-4">
-                我们帮助客户实现了业务数字化转型，取得了巨大的成功。
-              </p>
-              <a href="#" className="btn">
-                了解更多
-              </a>
-            </div>
+          <div className="flex flex-col justify-center items-center">
+            <img
+              src="/f2.png"
+              className="mb-4"
+            />
+            <p className="text-gray-600 mb-4">
+              用Midjourney，一分钟创建属于自己的角色
+            </p>
+            <Link className="underline text-center" href={'/midjourney'}>了解更多</Link>
           </div>
-
-          <div className="testimonials mb-10">
-            <h2 className="font-bold mb-4">客户评价</h2>
-            <div className="testimonial">
-              <blockquote className="mb-4">
-                “我们使用AI平台，取得了惊人的效果。它是我们数字化转型的关键。”
-              </blockquote>
-              <p className="text-gray-600 font-bold mb-2">
-                John Doe, CEO of ABC Company
-              </p>
-              <p className="text-gray-500">San Francisco, CA</p>
-            </div>
-            <div className="testimonial">
-              <blockquote className="mb-4">
-                “我们使用AI平台，取得了惊人的效果。它是我们数字化转型的关键。”
-              </blockquote>
-              <p className="text-gray-600 font-bold mb-2">
-                John Doe, CEO of ABC Company
-              </p>
-              <p className="text-gray-500">San Francisco, CA</p>
-            </div>
-            <div className="testimonial">
-              <blockquote className="mb-4">
-                “我们使用AI平台，取得了惊人的效果。它是我们数字化转型的关键。”
-              </blockquote>
-              <p className="text-gray-600 font-bold mb-2">
-                John Doe, CEO of ABC Company
-              </p>
-              <p className="text-gray-500">San Francisco, CA</p>
-            </div>
+          <div className="flex flex-col justify-center items-center">
+            <img
+              src="/f1.png"
+              className="mb-4"
+            />
+            <p className="text-gray-600 mb-4">用Midjourney，一分钟设计机具创意的LOGO</p>
+            <Link className="underline text-center" href={'/midjourney'}>了解更多</Link>
           </div>
         </div>
-      </main>
-    </div>
+      </div>
+      <Footer />
+    </main>
   );
 };
 
