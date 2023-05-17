44b749c48f5f66b2f94b7caa93303944ff303395
Aralhi <xiaoping0501cqu@163.com>
2023-05-17 19:16:23 +0800

    feat: 验证wx

--- pages/api/weichat/event.ts
+++ pages/api/weichat/event.ts
@@ -1,6 +1,21 @@
 import { NextApiRequest, NextApiResponse } from "next"
+import crypto from 'crypto'
+
+if (!process.env.WX_PUBLIC_TOKEN) {
+  throw new Error("Missing env var from WX");
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   console.log('weichat event', req.method, req.query, req.body)
-  return res.status(200).json({ status: 'ok', message: 'ok' })
+  if (req.method === 'GET') {
+    // 验证微信消息
+    const { signature, nonce, timestamp, echostr } = req.query || {}
+    const str = [process.env.WX_PUBLIC_TOKEN, timestamp, nonce].sort().join('')
+    const hash = crypto.createHash('sha1').update(str).digest('hex');
+    if (hash === signature) {
+      return res.status(200).send(echostr)
+    } else {
+      return res.status(403).json({ status: 'failed', message: 'forbidden' })
+    }
+  }
 }
