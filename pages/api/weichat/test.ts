2ddd071958ec69f1a3b8774d4676545a3bac8783
Aralhi <xiaoping0501cqu@163.com>
2023-05-18 01:05:08 +0800

    feat: add log

--- pages/api/weichat/test.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next"
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log('weichat event', process.env.WX_PUBLIC_TOKEN, req.method, req.query, req.body)
-  return res.json({ status: 'success' })
-}
