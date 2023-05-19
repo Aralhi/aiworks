4ec9e296337db494c6d1c98ef91a221c183bd728
Aralhi <xiaoping0501cqu@163.com>
2023-05-20 01:13:26 +0800

    fix: bug

--- pages/api/completion/index.ts
+++ pages/api/completion/index.ts
@@ -8,7 +8,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   // 查询会话下的问答列表
   if (!req.session.user || !req.session.user.isLoggedIn || !req.session.user._id) {
-    res.status(400).json({ status: 'failed', message: '您无法做此操作，请先登录' })    
+    return res.status(400).json({ status: 'failed', message: '您无法做此操作，请先登录' })    
   }
   if (req.method === 'GET') {
     try {
