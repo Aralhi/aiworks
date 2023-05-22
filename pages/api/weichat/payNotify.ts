294cc0b2dbf89b0d4bc7ca0eb46d40a993ec11fc
Zheng Ye <barrysfinalhome@outlook.com>
2023-05-22 15:24:31 +0800

    Update payNotify.ts

--- pages/api/weichat/payNotify.ts
+++ pages/api/weichat/payNotify.ts
@@ -8,13 +8,17 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   await dbConnect();
   await new WxEvent({
     type: WX_EVENT_TYPE.pay_notify,
+    value: JSON.stringify({
+      body: req.body,
+      headers: req.headers,
+    }),
     message: {
       body: req.body,
       headers: req.headers,
     },
     body: req.body,
     headers: req.headers,
-    time: new Date().getTime(),
+    createAt: new Date(),
   }).save()
   res.json({
     success: 1,
