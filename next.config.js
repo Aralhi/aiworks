c44c43d9dbccd228a9b7860cab325e22a0e2036f
Aralhi <xiaoping0501cqu@163.com>
2023-05-24 01:20:15 +0800

    feat: add stream header

--- next.config.js
+++ next.config.js
@@ -2,4 +2,17 @@
 
 module.exports = {
   reactStrictMode: false,
+  headers: async () => {
+    return [
+      {
+        source: "/api/chatgpt/get",
+        headers: [
+          {
+            key: "Transfer-Encoding",
+            value: "chunked",
+          }
+        ]
+      },
+    ];
+  },
 };
