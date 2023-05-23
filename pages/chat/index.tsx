6a2316f0f4c4225dca2e968ecd292609c348f9b8
Aralhi <xiaoping0501cqu@163.com>
2023-05-24 01:03:07 +0800

    feat: use node

--- pages/chat/index.tsx
+++ pages/chat/index.tsx
@@ -130,7 +130,7 @@ function Chat({ conversationList }: InferGetServerSidePropsType<typeof getServer
     ])
     setPrompt('') // 清空输入框
     try {
-      const response = await fetch('/api/chatgpt/get2', {
+      const response = await fetch('/api/chatgpt/get', {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
