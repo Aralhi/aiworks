e8eed76188511e1123e899a2e7d515725ebdc493
dengyun <dengyun@dengyundeMacBook-Pro.local>
2023-05-23 19:50:57 +0800

    update

--- pages/api/chatgpt/get2.ts
+++ pages/api/chatgpt/get2.ts
@@ -1,7 +1,12 @@
-import { OpenAIStream, OpenAIStreamPayload } from "../../../utils/OpenAIStream2";
+// import { withIronSessionApiRoute } from 'iron-session/next';
 import { getIronSession } from 'iron-session/edge';
+import { OpenAIStream, OpenAIStreamPayload } from "../../../utils/OpenAIStream2";
 import { sessionOptions } from "@/lib/session";
 import { NextApiRequest, NextApiResponse } from "next";
+import Conversation from '@/models/Conversation';
+import { FINGERPRINT_KEY, MAX_CONVERSATION_COUNT, MAX_TOKEN } from '@/utils/constants';
+import { checkQueryCount } from '@/lib/completion';
+import { random } from 'lodash';
 
 if (!process.env.OPENAI_API_KEY) {
   throw new Error("Missing env var from OpenAI");
@@ -25,6 +30,31 @@ console.log(session.user);
     return new Response("No prompt in the request", { status: 400 });
   }
 
+  const { _id: userId } = session.user || {}
+  // 校验queryCount
+  // const { status, message } = await checkQueryCount(req)
+  // if (status !== 'ok') {
+  //   return res.status(200).json({ status, message })
+  // }
+  // 没conversationId先创建一条conversation，后续的completion都关联到这个conversation
+  let newConversationId = '11111';
+  // // 登录了才创建会话
+  // if (userId && !conversationId && conversationName) {
+  //   // 查询历史会话格式
+  //   const count = await Conversation.countDocuments({ userId })
+  //   if (count < MAX_CONVERSATION_COUNT) {
+  //     try {
+  //       const newDoc = await Conversation.create({
+  //         userId,
+  //         name: conversationName,
+  //       })
+  //       console.log('insert conversation success:', newDoc)
+  //       newConversationId = newDoc._id
+  //     } catch (error) {
+  //       console.log('insert conversation error:', error)
+  //     }
+  //   }
+  // }
   const payload: OpenAIStreamPayload = {
     model: "gpt-3.5-turbo",
     messages: [{ role: "user", content: prompt }],
@@ -32,17 +62,18 @@ console.log(session.user);
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
-    max_tokens: 200,
-    stream: true,
+    max_tokens: !isStream ? MAX_TOKEN / 2 : MAX_TOKEN, // 非流式请求最大token数为流式请求的一半，防止一次请求返回太多数据，场景主要是微信调用
+    stream: !!isStream,
     n: 1,
   };
   const startTime = Date.now();
-  const stream = await OpenAIStream(payload);
-  console.log('stream...', stream);
+  // //TODO WX调用需要传用户信息
+  // const stream = await OpenAIStream({ payload, request: req, conversationId: conversationId || newConversationId, user: session.user });
+  const stream = await OpenAIStream(payload, req, session.user, conversationId || newConversationId);
   if (isStream) {
-    console.log('chatgpt response:', isStream, Date.now() - startTime)
     return new Response(stream);
   }
+
   const reader = stream.getReader();
   const decoder = new TextDecoder();
   let result = '';
@@ -61,6 +92,7 @@ console.log(session.user);
       'content-type': 'application/json'
     }
   })
+
 };
 
 export default handler;
--- utils/OpenAIStream2.ts
+++ utils/OpenAIStream2.ts
@@ -3,10 +3,16 @@ import {
   ParsedEvent,
   ReconnectInterval,
 } from "eventsource-parser";
+import { UserSession } from 'pages/api/user/user';
+import { FINGERPRINT_KEY } from "./constants";
+import cache from 'memory-cache'
+import { saveCompletion } from "@/lib/completion";
 
 const MAX_STRING_LENGTH = 2 * 1024 // 2K
+const CHAT_CONTEXT_PRE = 'chat_context_'
+const CHAT_CONTEXT_CACHE_TIME = 5 * 60 * 1000 // 1小时
 
-export type ChatGPTAgent = "user" | "system";
+export type ChatGPTAgent = "user" | "system" | "assistant";
 
 export interface ChatGPTMessage {
   role: ChatGPTAgent;
@@ -25,11 +31,23 @@ export interface OpenAIStreamPayload {
   n: number;
 }
 
-export async function OpenAIStream(payload: OpenAIStreamPayload) {
+interface chatContext {
+  question: string,
+  answer: string
+}
+
+export async function OpenAIStream(payload: OpenAIStreamPayload, request: any, user: any, conversationId: string) {
   const encoder = new TextEncoder();
   const decoder = new TextDecoder();
-
+   console.log('09090909', request.headers.get(FINGERPRINT_KEY))
   let counter = 0;
+
+  // 获取上下文记忆
+  const messages = getContext(conversationId, request.headers.get(FINGERPRINT_KEY));
+
+console.log(messages, '0000000')
+
+  payload.messages = messages.concat(payload.messages);
 
   function getResponseByProd() {
     return fetch("https://api.openai.com/v1/chat/completions", {
@@ -56,6 +74,10 @@ export async function OpenAIStream(payload: OpenAIStreamPayload) {
   }
 
   const res = process.env.NODE_ENV === 'development' ? await getResponseByDev() : await getResponseByProd()
+
+  let contents: Array<string> = []
+  let chatId: string = ''
+
   const stream = new ReadableStream({
     async start(controller) {
       // callback
@@ -65,18 +87,21 @@ export async function OpenAIStream(payload: OpenAIStreamPayload) {
           // https://beta.openai.com/docs/api-reference/completions/create#completions/create-stream
           if (data === "[DONE]") {
             controller.close();
+            // completionCallback({ payload, request, content: contents.join(''), user, chatId, conversationId })
             return;
           }
           try {
             const json = JSON.parse(data);
+            chatId = json.id
             const text = json.choices[0].delta?.content || "";
-            if (counter < 2 && (text.match(/\n/) || []).length) {
-              // this is a prefix character (i.e., "\n\n"), do nothing
-              return;
-            }
+            // if (counter < 2 && (text.match(/\n/) || []).length) {
+            //   // this is a prefix character (i.e., "\n\n"), do nothing
+            //   return;
+            // }
+            contents.push(text);
             const queue = encoder.encode(text);
             controller.enqueue(queue);
-            counter++;
+            // counter++;
           } catch (e) {
             // maybe parse error
             controller.error(e);
@@ -95,3 +120,75 @@ export async function OpenAIStream(payload: OpenAIStreamPayload) {
   });
   return stream
 }
+
+function getContext(conversationId: string, fingerprint: string) {
+  let messages: ChatGPTMessage[] = []
+  if (!conversationId && !fingerprint) {
+    return []
+  }
+  // 未登录用户用哦fingerPrint做为key，让用户享受到上下文功能
+  const key = `${CHAT_CONTEXT_PRE}${conversationId || fingerprint}`
+  const chatContextArr: chatContext[] =  cache.get(key) || [];
+  // 取最近两个问题做为上下文记忆, 如果考虑节省token，可以取最近一个问题做为上下文记忆
+  if (chatContextArr.length < 1) {
+    return []
+  }
+  const lastTwoQuestions: chatContext[] = chatContextArr.slice(-2);
+  lastTwoQuestions.forEach((ele: chatContext) => {
+    messages.push({
+      role: 'user',
+      content: ele.question
+    }, {
+      role: 'assistant',
+      content: ele.answer
+    });
+  });
+  return messages
+}
+
+// function setContext(conversationId: string, fingerprint: string, question: string, answer: string) {
+//   if (!conversationId && !fingerprint) {
+//     return []
+//   }
+//   const key = `${CHAT_CONTEXT_PRE}${conversationId || fingerprint}`
+//   const chatContextArr: chatContext[] =  cache.get(key) || [];
+//   chatContextArr.push({
+//     question,
+//     answer
+//   });
+//   //TODO: 考虑文本内容可能超大，设置较短缓存过期时间，过期从数据库中读取。
+//   cache.put(key, chatContextArr, CHAT_CONTEXT_CACHE_TIME)
+//   console.log('set cache success', key)
+// }
+
+// async function completionCallback({ payload, request, content, user, chatId, conversationId, usage }: {
+//   payload: OpenAIStreamPayload,
+//   request: any
+//   content: string,
+//   user?: UserSession,
+//   chatId: string,
+//   conversationId: string,
+//   usage?: any
+// }) {
+//   try {
+//     console.log('completionCallback', payload)
+//     // 内容全部返回完成, 将本次返回内容记录到缓存
+//     const fingerprint = user?._id ? '' : request.headers.get(FINGERPRINT_KEY);
+//     setContext(conversationId, fingerprint, payload.messages[payload.messages.length - 1].content, content)
+//     // 未登录用户记录用户的fingerPrint
+//     await saveCompletion({
+//       userId: user?._id || '',
+//       prompt: payload.messages[payload.messages.length - 1].content,
+//       role: payload.messages[0].role,
+//       stream: payload.stream,
+//       chatId,
+//       model: payload.model,
+//       conversationId,
+//       content,
+//       usage,
+//       fingerprint
+//     })
+//   } catch (e) {
+//     console.error('insert completion failed', e)
+//   }
+// }
