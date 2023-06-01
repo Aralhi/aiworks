79044e4254c00bb36078556224b83a0d4a343cc6
hzh97 <2976151305@qq.com>
2023-06-02 01:44:01 +0800

    docs: 指引内容简化

--- components/MJOptsPanel/MJExplain.tsx
+++ components/MJOptsPanel/MJExplain.tsx
@@ -38,13 +38,13 @@ function MJExplain() {
   return (
     <div className="p-6 w-full rounded-lg bg-white text-slate-900 text-justify shadow-lg">
       <h1 className="w-full text-center font-bold text-xl mt-2 mb-8">Midjourney上手教程</h1>
-      <Row className="mb-8">
+      {/* <Row className="mb-8">
         <h2 className="text-gray-800 text-lg mb-4 w-full font-medium">什么是Midjourney</h2>
         <span>
           Midjourney是一款AI制图工具，只要关键字，就能透过AI算法生成相对应的图片，只需要不到一分钟。
           可以选择不同画家的艺术风格，例如安迪华荷、达芬奇、达利和毕加索等，还能识别特定镜头或摄影术语。
         </span>
-      </Row>
+      </Row> */}
       <Row className="mb-8">
         <h2 className="text-gray-800 text-lg mb-4 w-full font-medium">如何使用</h2>
         <div className="space-y-2">
@@ -53,15 +53,15 @@ function MJExplain() {
             例如：<Tag>A dog in space</Tag>(只支持全英文输入)，会输出以下结果
             <ArrowDownOutlined rev="" />
           </p>
-          <Image className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 rounded-lg shadow-lg" width={300} height={300} src='/case-1.jpg' alt="" />
+          <Image className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 rounded-lg shadow-lg" width={300} height={300} src="/case-1.jpg" alt="" />
           <p>
             也可以输入更多生成参数，例如：<Tag>A dog in space --v 4</Tag>，其中<Tag>--v 4</Tag>为使用模型V4版本
           </p>
-          <p>
+          {/* <p>
             更多参数可参考以下表格
             <ArrowDownOutlined rev="" />
           </p>
-          <Table pagination={false} bordered columns={columns} dataSource={data}></Table>
+          <Table pagination={false} bordered columns={columns} dataSource={data}></Table> */}
           <p>
             <Tag>U1 ~ U4</Tag>选项说明：选定图1 - 4任意一张输出图片
           </p>
