1d22df1229fba4256df7149f51eccc162db44a60
hzh97 <2976151305@qq.com>
2023-06-04 13:22:12 +0800

    feat: 允许http请求

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
@@ -6,15 +6,10 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="description"
-            content="AIworks，让AI触手可及。"
-          />
+          <meta name="description" content="AIworks，让AI触手可及。" />
+          <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
           <meta property="og:site_name" content="aiworks.club" />
-          <meta
-            property="og:description"
-            content="AIworks，让AI触手可及。"
-          />
+          <meta property="og:description" content="AIworks，让AI触手可及。" />
           <meta property="og:title" content="AIworks" />
         </Head>
         <body>
