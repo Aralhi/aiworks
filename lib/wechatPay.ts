4f74b0f0b9860f045b4a134057650c34e5277bbf
Zheng Ye <barrysfinalhome@outlook.com>
2023-06-05 19:44:26 +0800

    Update wechatPay.ts
    Change to string

--- lib/wechatPay.ts
+++ lib/wechatPay.ts
@@ -88,7 +88,7 @@ function getPaySign(appid: string, body: string) {
   ]).build();
   return {
     appId: appid,
-    timeStamp: timestamp,
+    timeStamp: `${timestamp}`,
     nonceStr,
     package: body,
     paySign,
